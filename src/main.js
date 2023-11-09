31bca0c3ac38ac5fff81698235ae2bf2cd85e8e0
Choti <jgchotirat@gmail.com>
2023-11-09 12:43:07 +0700

    usr try..check when click the '%' button

--- src/main.js
+++ src/main.js
@@ -16,13 +16,22 @@ function filterInput(input) {
 
 // Display value when click buttons
 function displayValue(value) {
+    // Check if the button click is the '%' button
     if (value === '%' && displayField.value.length > 0) {
         // Remove = in front of the input
         if (displayField.value[0] === '=' || displayField.value[0] === '*' || displayField.value[0] === '/') {
             displayField.value = displayField.value.slice(1)
         }
-        displayField.value = (parseFloat(displayField.value)) / 100;
+        try {
+            // Check if the value is a number and calculate its percentage
+            if (!isNaN(parseFloat(displayField.value) / 100)) {
+                displayField.value = '=' + (parseFloat(displayField.value)) / 100
+            }
+        } catch (error) {
+            checkForErrors(inputValue);
+        }
     } else {
+        // If the button click is not '%', display the value to the displayField
         displayField.value += value;
     }
 }
@@ -47,26 +56,29 @@ function calculateResult() {
         inputValue = inputValue.slice(1)
         console.log(inputValue)
     }
-    // Display Error if the input is empty.
-    if (inputValue.length === 0) {
-        displayField.value = 'Error: Invalid input';
-        return;
-    }
-    //  Display Error if the input is start or end with operators.
-    if (inputValue[0] === '/' || inputValue[0] === '*' || inputValue[0] === '%' || inputValue[inputValue.length - 1] == '/' || inputValue[inputValue.length - 1] == '*' || inputValue[inputValue.length - 1] === '%') {
-        displayField.value = 'Error: Can not start or end with operators'
-        return;
-    }
+
     // Display result if the result is numbers.
     try {
         if (!isNaN(eval(inputValue))) {
             displayField.value = '=' + eval(inputValue).toFixed(2)
         }
-        // Display Error if the result is invalid.
-        else {
-            displayField.value = 'Error: Invalid result'
-        }
     } catch (error) {
+        checkForErrors(inputValue);
+    }
+}
+
+// Check for error and display to users
+function checkForErrors(inputValue) {
+    // Display Error if the input is empty.
+    if (inputValue.length === 0) {
+        displayField.value = 'Error: Invalid input'
+        return;
+    }
+    //  Display Error if the input is start or end with operators.
+    else if (inputValue[0] === '/' || inputValue[0] === '*' || inputValue[0] === '%' || inputValue[inputValue.length - 1] == '/' || inputValue[inputValue.length - 1] == '*' || inputValue[inputValue.length - 1] === '%') {
+        displayField.value = 'Error: Can not start or end with operators';
+        return;
+    } else {
         displayField.value = 'Error: Invalid input'
     }
 }
